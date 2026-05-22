8c68c978452cf462bd26d0be4db16daf9fda5941
Moises Rojas <ssj52949@gmail.com>
2026-05-22 00:38:07 -0500

    dwqdqwdeqwe

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,4 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  // eslint-disable-next-line prettier/prettier
-// eslint-disable-next-line prettier/prettier
 });
